04f6f82b93f0dd6aeee68d99054188423ec21a04
lamenking <dnwjddus1506@hanmail.net>
2021-09-02 22:01:22 +0900

    🐛 Fix disable prettier about report file

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,4 @@
+/*disable prettier*/
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
