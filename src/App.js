e32b459465db445e0f4c67c2529b43c81541c3d5
lamenking <dnwjddus1506@hanmail.net>
2021-09-03 10:05:12 +0900

    🚧  Working SearchInput

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import HeaderGnb from './components/HeaderGnb';
 
 import './styles/reset.css';
 import './styles/gnb.css';
+import './styles/search-input.css';
 
 function App() {
   return (
-    <>
+    <div className="container">
       <HeaderGnb></HeaderGnb>
-    </>
+    </div>
   );
 }
 
