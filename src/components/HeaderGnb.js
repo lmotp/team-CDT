d0201040c2ecba8554018a789be97e4acf9575ac
lamenking <dnwjddus1506@hanmail.net>
2021-09-03 10:04:08 +0900

    📝 react Typo correction 'r' → 'R'

--- src/components/HeaderGnb.js
+++ src/components/HeaderGnb.js
@@ -1,15 +1,23 @@
-import react, { useState } from 'react';
+import React, { useState } from 'react';
 import GnbTitle from './GnbTitle.js';
 import SearchInput from './SearchInput.js';
 import WriteButton from './WriteButton.js';
 import GnbMenu from './GnbMenu.js';
 
 export default function HeaderGnb() {
+  const [inputValue, setInputValue] = useState('');
+  const handleInputValue = (e) => {
+    setInputValue(e.target.value);
+  };
+  const handleSearchButton = () => {
+    setInputValue('');
+  };
+
   return (
     <>
       <header className="gnb">
         <GnbTitle></GnbTitle>
-        <SearchInput></SearchInput>
+        <SearchInput value={inputValue} onChange={handleInputValue} onClick={handleSearchButton}></SearchInput>
         <WriteButton></WriteButton>
         <GnbMenu></GnbMenu>
       </header>
