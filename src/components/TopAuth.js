97d09945fc55327676c66c2c9d7e71c5d9862d9a
lamenking <dnwjddus1506@hanmail.net>
2021-09-06 22:58:01 +0900

    🌱 Update aria-label and visually-hidden

--- src/components/TopAuth.js
+++ src/components/TopAuth.js
@@ -5,17 +5,19 @@ export default function TopAuth() {
     <>
       <div className="top-auth">
         <div className="inner">
-          <button>
+          <button type="text" aria-label="유저 로그인 버튼">
             <i class="far fa-user user-icon"></i>
           </button>
-          <button className="login">로그인</button>
+          <button type="text" className="login">
+            로그인
+          </button>
         </div>
       </div>
 
       {/* 로그인 후
       <div className="top-auth">
         <div className="inner">
-          <a href="/" className="profile">
+          <a href="/" className="profile" aria-label="유저 프로필">
             <i class="far fa-user-circle user-icon"></i>
             <span className="nickname">닉네임</span>
           </a>
