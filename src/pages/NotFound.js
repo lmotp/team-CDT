d430dcc31f16f65f1acb1d4dc5e42b41e62eb510
lmotp <lmotp@daum.net>
2021-09-08 19:16:27 +0900

    Route Add && NotFound ⛑

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div className="notFound-wrap">
+      <h1>404 페이지를 찾을 수 없습니다.</h1>
+    </div>
+  );
+}
+
+export default NotFound;
