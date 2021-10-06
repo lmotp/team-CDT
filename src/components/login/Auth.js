b7238fac437987a896d2263b55d83f67958d9e17
lamenking <dnwjddus1506@hanmail.net>
2021-10-06 17:10:42 +0900

    🚧 Working auth varidation

--- src/components/login/Auth.js
+++ src/components/login/Auth.js
@@ -11,78 +11,190 @@ function Auth({ history }) {
 
   const [selectGenderValue, setSelectGenderValue] = useState('');
   const [selectMonthValue, setSelectMonthValue] = useState('');
-  const [selectYearValue, setSelectYearValue] = useState('');
+  const [joinData, setJoinData] = useState(false);
 
   const genderRef = useRef();
   const monthRef = useRef();
 
-  const usernameFs = useFocus(false);
-  const pwdFs = useFocus(false);
-  const checkPwdFs = useFocus(false);
-  const nameFs = useFocus(false);
+  const testCheck = (rgx, data, dataFs) => {
+    const checkRgx = rgx;
+    if (checkRgx.test(data.inputValue) /*|| data.inputValue.length === 0*/) {
+      dataFs.setTest(true);
+    } else if (!checkRgx.test(data.inputValue) /*&& data.inputValue.length !== 0*/) {
+      dataFs.setTest(false);
+    }
+  };
+
+  const yearTestCheck = () => {
+    const checkRgx = /^[0-9]{4}$/;
+    if (checkRgx.test(bYear.inputValue) && bYear.inputValue.length === 4) {
+      bYearFs.setFocus(false);
+      bMonthFs.setFocus(true);
+      bYearFs.setTest(true);
+    } else {
+      bYearFs.setFocus(true);
+      bMonthFs.setFocus(false);
+    }
+  };
+
+  const dayTestCheck = () => {
+    const checkRgx = /^[0-9]{1,2}$/;
+    if (
+      checkRgx.test(bDay.inputValue) === true &&
+      (bDay.inputValue.length === 1 || bDay.inputValue.length === 2) &&
+      bDay.inputValue <= 31
+    ) {
+      bYearFs.setFocus(false);
+      bMonthFs.setFocus(false);
+      bDayFs.setFocus(false);
+      bDayFs.setTest(true);
+    } else if ((checkRgx.test(bDay.inputValue) === false && bYear.inputValue.length !== 0) || bDay.inputValue >= 31) {
+      bYearFs.setFocus(false);
+      bMonthFs.setFocus(false);
+      bDayFs.setFocus(true);
+    }
+    {
+      /*else if (
+      checkRgx.test(bDay.inputValue) === false &&
+      bYear.inputValue.length !== 0 &&
+      monthRef.current.value === 'month'
+    ) {
+      bYearFs.setFocus(true);
+      bMonthFs.setFocus(false);
+      bDayFs.setFocus(false);
+    }*/
+    }
+  };
+
+  const rePwdTestCheck = () => {
+    if (pwd.inputValue !== checkPwd.inputValue && checkPwd.inputValue.length !== 0) {
+      checkPwdFs.setTest(false);
+    } else {
+      checkPwdFs.setTest(true);
+    }
+  };
+
+  const usernameFs = useFocus(false, () => {
+    return testCheck(/^[a-zA-Z0-9]{5,20}$/gm, username, usernameFs);
+  });
+  const pwdFs = useFocus(false, () => {
+    return testCheck(/^[a-zA-Z0-9!@#$%^&*]{8,16}$/gm, pwd, pwdFs);
+  });
+  const checkPwdFs = useFocus(false, () => {
+    return rePwdTestCheck();
+  });
+  const nameFs = useFocus(false, () => {
+    return testCheck(/^[가-힣a-zA-Z]{1,20}$/, name, nameFs);
+  });
   const genderFs = useFocus(false);
   const bYearFs = useFocus(false);
   const bMonthFs = useFocus(false);
   const bDayFs = useFocus(false);
-  const pNFs = useFocus(false);
+  const pNFs = useFocus(false, () => {
+    return testCheck(/^[0-9]{9,11}$/, phoneNumber, pNFs);
+  });
 
-  const username = useInput('', usernameFs.setFocus);
-  const pwd = useInput('', pwdFs.setFocus);
-  const checkPwd = useInput('', checkPwdFs.setFocus);
-  const name = useInput('', nameFs.setFocus);
-  const birthdayDay = useInput('', bDayFs.setFocus);
-  const phoneNumber = useInput('', pNFs.setFocus);
+  const username = useInput('', usernameFs.setFocus, usernameFs.setTest);
+  const pwd = useInput('', pwdFs.setFocus, pwdFs.setTest);
+  const checkPwd = useInput('', checkPwdFs.setFocus, checkPwdFs.setTest);
+  const name = useInput('', nameFs.setFocus, nameFs.setTest);
+  const bYear = useInput('', bYearFs.setFocus, bYearFs.setTest);
+  const bDay = useInput('', bDayFs.setFocus, bDayFs.setTest);
+  const phoneNumber = useInput('', pNFs.setFocus, pNFs.setTest);
 
   const selectGenderChange = (e) => {
+    // 성별을 변경하였을 때 바로 동작 (select)
     setSelectGenderValue(e.target.value);
     if (e.target.value !== 'gender') {
       genderFs.setFocus(false);
+      genderFs.setTest(true);
     } else if (e.target.value === 'gender') {
       genderFs.setFocus(true);
     }
   };
 
+  const selectMonthChange = (e) => {
+    // 생년월일 '월'을 변경하였을 때 바로 동작 (select)
+    setSelectMonthValue(e.target.value);
+    if (e.target.value !== 'month' && bYear.inputValue.length === 0) {
+      bDayFs.setFocus(false);
+      bMonthFs.setFocus(false);
+      bYearFs.setFocus(true);
+    } else if (e.target.value === 'month' && bYear.inputValue.length !== 0) {
+      bYearFs.setFocus(false);
+      bDayFs.setFocus(false);
+      bMonthFs.setFocus(true);
+    } else if (e.target.value !== 'month' && bDay.inputValue.length === 0 && bYear.inputValue.length !== 0) {
+      bYearFs.setFocus(false);
+      bMonthFs.setFocus(false);
+      bDayFs.setFocus(true);
+      bMonthFs.setTest(true);
+    } else if (monthRef.current.value !== 'month' && bDay.inputValue.length !== 0 && bYear.inputValue.length !== 0) {
+      bYearFs.setFocus(false);
+      bMonthFs.setFocus(false);
+      bDayFs.setFocus(false);
+      bMonthFs.setTest(true);
+    }
+  };
+
   const blurGender = () => {
+    // 성별에서 blur를 발생시켰을 때
     if (genderRef.current.value === 'gender') {
       genderFs.setFocus(true);
     }
   };
 
-  const selectYearChange = (e) => {
-    setSelectYearValue(e.target.value);
-    if (e.target.value.length !== 0) {
-      bYearFs.setFocus(false);
-    }
-  };
-
   const blurYear = () => {
-    if (selectYearValue.length === 0) {
+    // 생년월일 '년'에서 blur를 발생시켰을 때
+    if (bYear.inputValue.length !== 4) {
       bYearFs.setFocus(true);
-    }
-    if (monthRef.current.value === 'month' && selectYearValue.length !== 0) {
-      bMonthFs.setFocus(true);
-    }
-  };
-
-  const selectMonthChange = (e) => {
-    setSelectMonthValue(e.target.value);
-    if (e.target.value !== 'month') {
       bMonthFs.setFocus(false);
-    } else if (e.target.value === 'month') {
+      bDayFs.setFocus(false);
+    } else if (bYear.inputValue.length === 4 && monthRef.current.value === 'month') {
+      bYearFs.setFocus(false);
+      bDayFs.setFocus(false);
       bMonthFs.setFocus(true);
-    }
-    if (birthdayDay.inputValue.length === 0) {
+    } else if (bYear.inputValue.length === 4 && monthRef.current.value !== 'month' && bDay.inputValue.length === 0) {
+      bYearFs.setFocus(false);
+      bMonthFs.setFocus(false);
       bDayFs.setFocus(true);
     }
+    yearTestCheck();
   };
 
   const blurMonth = () => {
-    if (selectYearValue.length !== 0 && monthRef.current.value === 'month') {
+    // 생년월일 '월'에서 blur를 발생시켰을 때
+    if (monthRef.current.value === 'month' && bYear.inputValue.length !== 0) {
+      bYearFs.setFocus(false);
+      bDayFs.setFocus(false);
       bMonthFs.setFocus(true);
+    } else if (monthRef.current.value !== 'month' && bDay.inputValue.length === 0 && bYear.inputValue.length !== 0) {
+      bYearFs.setFocus(false);
+      bMonthFs.setFocus(false);
+      bDayFs.setFocus(true);
+    } else if (monthRef.current.value !== 'month' && bYear.inputValue.length === 0) {
+      bDayFs.setFocus(false);
+      bMonthFs.setFocus(false);
+      bYearFs.setFocus(true);
+    } else if (monthRef.current.value !== 'month' && bDay.inputValue.length !== 0 && bYear.inputValue.length !== 0) {
+      bYearFs.setFocus(false);
+      bMonthFs.setFocus(false);
+      bDayFs.setFocus(false);
     }
-    if (monthRef.current.value !== 'month' && birthdayDay.inputValue.length === 0) {
+  };
+
+  const blurDay = () => {
+    // 생년월일 '일'에서 blur를 발생시켰을 때
+    if (bDay.inputValue.length !== 0 && monthRef.current.value !== 'month' && bYear.inputValue.length !== 0) {
+      bYearFs.setFocus(false);
+      bMonthFs.setFocus(false);
+      bDayFs.setFocus(false);
+    } else if (bDay.inputValue.length === 0 && monthRef.current.value !== 'month' && bYear.inputValue.length !== 0) {
+      bYearFs.setFocus(false);
+      bMonthFs.setFocus(false);
       bDayFs.setFocus(true);
     }
+    dayTestCheck();
   };
 
   const submitJoin = async (e) => {
@@ -94,9 +206,9 @@ function Auth({ history }) {
         checkPwd: checkPwd.inputValue,
         name: name.inputValue,
         gender: selectGenderValue,
-        birthdayYear: selectYearValue,
+        birthdayYear: bYear.inputValue,
         birthdayMonth: selectMonthValue,
-        birthdayDay: birthdayDay.inputValue,
+        birthdayDay: bDay.inputValue,
         phoneNumber: phoneNumber.inputValue,
       })
       .then((res) => {
@@ -104,6 +216,40 @@ function Auth({ history }) {
       });
     history.push('/user');
     window.scrollTo(0, 0);
+  };
+
+  const joinButton = (e) => {
+    if (
+      usernameFs.test === true &&
+      pwdFs.test === true &&
+      checkPwdFs.test === true &&
+      nameFs.test === true &&
+      genderFs.test === true &&
+      bYearFs.test === true &&
+      bMonthFs.test === true &&
+      bDayFs.test === true &&
+      pNFs.test === true
+    ) {
+      console.log('가입완료');
+      setJoinData(true);
+    } else {
+      window.alert('정보를 다시 입력해주세요.');
+      console.log('가입실패 : 유효성 검사 인증 실패로 인한');
+      window.scrollTo(0, 0);
+      history.push('/auth');
+      setJoinData(false);
+    }
+    console.log(
+      'test : ' + usernameFs.test,
+      pwdFs.test,
+      checkPwdFs.test,
+      nameFs.test,
+      genderFs.test,
+      bYearFs.test,
+      bMonthFs.test,
+      bDayFs.test,
+      pNFs.test,
+    );
   };
 
   return (
@@ -125,27 +271,33 @@ function Auth({ history }) {
               onBlur={usernameFs.handleFocus}
             ></input>
             {authData.length === 0 || authData[0].username !== username.inputValue ? null : (
-              <span className="idError">이미 사용중이거나 탈퇴한 아이디입니다.</span>
+              <span className="inputError">이미 사용중이거나 탈퇴한 아이디입니다.</span>
             )}
-            {usernameFs.focus ? <span className="idError">필수정보입니다.</span> : null}
+            {usernameFs.focus ? <span className="inputError">필수정보입니다.</span> : null}
+            {usernameFs.test || username.inputValue.length === 0 ? null : (
+              <span className="inputError">5~20자리의 영문 대소문자와 숫자만 사용가능합니다.</span>
+            )}
           </div>
           <div className="auth-content-list-item">
             <label for="pwd">비밀번호</label>
             <input
-              type="text"
+              type="password"
               id="pwd"
               name="pwd"
               value={pwd.inputValue}
               onChange={pwd.handleInputValue}
-              maxLength="20"
+              maxLength="16"
               onBlur={pwdFs.handleFocus}
             ></input>
-            {pwdFs.focus ? <span className="idError">필수정보입니다.</span> : null}
+            {pwdFs.focus ? <span className="inputError">필수정보입니다.</span> : null}
+            {pwdFs.test || pwd.inputValue.length === 0 ? null : (
+              <span className="inputError">8~16자리의 영문 대소문자와 숫자, 특수문자만 사용가능합니다.</span>
+            )}
           </div>
           <div className="auth-content-list-item">
             <label for="checkPwd">비밀번호 재확인</label>
             <input
-              type="text"
+              type="password"
               id="checkPwd"
               name="checkPwd"
               value={checkPwd.inputValue}
@@ -153,7 +305,10 @@ function Auth({ history }) {
               maxLength="20"
               onBlur={checkPwdFs.handleFocus}
             ></input>
-            {checkPwdFs.focus ? <span className="idError">필수정보입니다.</span> : null}
+            {checkPwdFs.focus ? <span className="inputError">필수정보입니다.</span> : null}
+            {checkPwdFs.test || checkPwd.inputValue.length === 0 ? null : (
+              <span className="inputError">비밀번호가 일치하지 않습니다.</span>
+            )}
           </div>
           <div className="auth-content-list-item">
             <label for="name">이름</label>
@@ -166,7 +321,10 @@ function Auth({ history }) {
               maxLength="20"
               onBlur={nameFs.handleFocus}
             ></input>
-            {nameFs.focus ? <span className="idError">필수정보입니다.</span> : null}
+            {nameFs.focus ? <span className="inputError">필수정보입니다.</span> : null}
+            {nameFs.test || name.inputValue.length === 0 ? null : (
+              <span className="inputError">한글과 영문 대소문자를 사용하세요.(특수문자, 공백 불가)</span>
+            )}
           </div>
           <div className="auth-content-list-item">
             <p>성별</p>
@@ -178,7 +336,7 @@ function Auth({ history }) {
               <option value="women">여자</option>
             </select>
             <i class="fas fa-angle-down gender-icon"></i>
-            {genderFs.focus ? <span className="idError">필수정보입니다.</span> : null}
+            {genderFs.focus ? <span className="inputError">필수정보입니다.</span> : null}
           </div>
           <div className="auth-content-list-item">
             <label for="yy" className="yy-header">
@@ -191,8 +349,8 @@ function Auth({ history }) {
                 id="yy"
                 name="yy"
                 placeholder="년(4자)"
-                value={selectYearValue}
-                onChange={selectYearChange}
+                value={bYear.inputValue}
+                onChange={bYear.handleInputValue}
                 className="yy"
                 aria-label="년(4자)"
                 onBlur={blurYear}
@@ -220,20 +378,16 @@ function Auth({ history }) {
                 maxLength="2"
                 name="dd"
                 placeholder="일"
-                value={birthdayDay.inputValue}
-                onChange={birthdayDay.handleInputValue}
+                value={bDay.inputValue}
+                onChange={bDay.handleInputValue}
                 className="dd"
                 aria-label="일"
-                onBlur={bDayFs.handleFocus}
+                onBlur={blurDay}
               ></input>
             </div>
-            {bYearFs.focus ? <span className="idError">태어난 년도를 정확하게 입력해주세요.</span> : null}
-            {bMonthFs.focus && !bYearFs.focus ? (
-              <span className="idError">태어난 월을 정확하게 입력해주세요</span>
-            ) : null}
-            {bDayFs.focus && !bMonthFs.focus && !bYearFs.focus ? (
-              <span className="idError">태어난 일을 정확하게 입력해주세요</span>
-            ) : null}
+            {bYearFs.focus === true ? <span className="inputError">태어난 년도를 정확하게 입력해주세요.</span> : null}
+            {bMonthFs.focus === true ? <span className="inputError">태어난 월을 정확하게 입력해주세요.</span> : null}
+            {bDayFs.focus === true ? <span className="inputError">태어난 일을 정확하게 입력해주세요.</span> : null}
           </div>
           <div className="auth-content-list-item">
             <label for="phoneNumber">휴대전화</label>
@@ -247,7 +401,10 @@ function Auth({ history }) {
               maxLength="11"
               onBlur={pNFs.handleFocus}
             ></input>
-            {pNFs.focus ? <span className="idError">필수정보입니다.</span> : null}
+            {pNFs.focus ? <span className="inputError">필수정보입니다.</span> : null}
+            {pNFs.test || phoneNumber.inputValue.length === 0 ? null : (
+              <span className="inputError">9자리 이상이며, 숫자만 가능합니다.</span>
+            )}
           </div>
           {/*
           <p className="auth-content-list-item">
@@ -265,7 +422,7 @@ function Auth({ history }) {
             </button>
           </p>
           */}
-          <button type="submit" className="join-button">
+          <button type={joinData ? 'submit' : 'button'} className="join-button" onClick={joinButton}>
             가입하기
           </button>
         </form>
