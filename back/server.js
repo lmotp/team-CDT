8620cbacfc1d091b2a9d2f311667b25f69def553
lamenking <dnwjddus1506@hanmail.net>
2021-11-12 12:48:00 +0900

    make it better

--- back/server.js
+++ back/server.js
@@ -41,25 +41,7 @@ app.use(
   }),
 );
 
-function handleDisconnect() {
-  connection.connect(function (err) {
-    if (err) {
-      console.log('error when connecting to connection:', err);
-      setTimeout(handleDisconnect, 2000);
-    }
-  });
-
-  connection.on('error', function (err) {
-    console.log('connection error', err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      return handleDisconnect();
-    } else {
-      throw err;
-    }
-  });
-}
-
-handleDisconnect();
+connection.connect();
 
 const upload = multer({
   storage: multer.diskStorage({
