385b4e21d81c101394ef7df9e0c87b2e17964d61
lmotp <lmotp@daum.net>
2021-11-15 12:47:52 +0900

    make

--- back/server.js
+++ back/server.js
@@ -22,13 +22,13 @@ const sessionStore = new MysqlStore(options);
 
 const port = process.env.PORT || 5000;
 
-const connection = mysql.createConnection({
+const db_config = {
   host: process.env.DB_HOST,
   port: process.env.DB_HOSTPORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-});
+};
 
 app.use(cookieParser());
 app.use(
@@ -41,13 +41,28 @@ app.use(
   }),
 );
 
-connection.on('error', function (err) {
-  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-    return connection.connect();
-  } else {
-    throw err;
-  }
-});
+let connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+
+  connection.connect(function (err) {
+    if (err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+  connection.on('error', function (err) {
+    console.log('db error', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 const upload = multer({
   storage: multer.diskStorage({
